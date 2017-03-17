c567a7c724877f156eefe5a814ae948cdeaca3fc
Kali Uday Balleda <kalib@xilinx.com>
2017-03-17 12:07:14 +0530

    [row_array_2d]: Improved makefile and utility directory added

--- utility/run.sh
+++ utility/run.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+if [ -f "$PWD/_sds/init.sh" ] 
+then
+rm -rf $PWD/_sds/emulation/sd_card
+sdsoc_emulator -no-reboot |tee emulator.log
+else
 cat > "init.sh" <<EOT
-#!/bin/sh
+!/bin/sh
 /mnt/$1
 reboot
 EOT
 echo $PWD/_sds/init.sh >> "_sds/emulation/sd_card.manifest"
 mv init.sh _sds
-mv *.jou _sds
-mv *.log _sds
-mv src/*.o _sds
 sdsoc_emulator -no-reboot |tee emulator.log
- 
+fi
