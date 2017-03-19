db4b7c1170106158e277d8dcfa353a6d87801afa
Kali Uday Balleda <kalib@xilinx.com>
2017-03-20 01:41:26 +0530

    [Automake] Advanced version cleaned up fully

--- utility/makefile_gen/makegen.py
+++ utility/makefile_gen/makegen.py
@@ -5,6 +5,15 @@ import os
 import subprocess
 
 def create_subdir_mk(target, data):
+    target.write("SDSCC := sds++\n")
+    target.write("\n")
+    target.write("OS := -sds-sys-config ")
+    os_config = data.get('os')
+    target.write(os_config)
+    target.write("\n")
+    target.write("\n")
+    target.write("PLATFORM := -sds-pf\n")
+    target.write("\n")
     elem_count = len(data["src_files"])
     target.write("CPP_SRCS += \\\n")
     for result in data["src_files"]:
@@ -44,13 +53,13 @@ def create_subdir_mk(target, data):
     target.write("\t @echo 'Building file: $<' \n")
     target.write("\t @echo 'Invoking: SDS++ Compiler' \n")
     target.write("\t")
-    target.write("sds++ -Wall -O0 -g -I\"../src\" -c -fmessage-length=0 -MT\"$@\" -MMD -MP -MF\"$(@:%.o=%.d)\" -MT\"$(@)\" -o \"$@\" \"$<\" -sds-hw ")
+    target.write("$(SDSCC) -Wall -O0 -g -I\"../src\" -c -fmessage-length=0 -MT\"$@\" -MMD -MP -MF\"$(@:%.o=%.d)\" -MT\"$(@)\" -o \"$@\" \"$<\" -sds-hw ")
     accel = data.get("hw_function")
     target.write(accel)
     target.write(" ")
     accel_file = data.get("accel_file")
     target.write(accel_file)
-    target.write(" -clkid 1 -sds-end -sds-sys-config linux -sds-proc a9_0 -sds-pf ")
+    target.write(" -clkid 1 -sds-end $(OS) $(PLATFORM) ")
     device = data.get("device")
     target.write("\"")
     target.write(device)
@@ -61,55 +70,75 @@ def create_subdir_mk(target, data):
     return
 
 def create_mk(target, data, emu_switch):
-    target.write("RM := rm -rf _sds sd_card\n")
-    target.write("# All the sources participating in build are defined here\n")
-    target.write("-include src/subdir.mk")
+
+    target.write("# Sources are Converted into Objects\n")
+    target.write("-include src/srcobj.mk")
     target.write("\n")
-    target.write("-include subdir.mk")
+   
     target.write("\n")
     
-    target.write("# All Target\n")
-    target.write("all: pre-build main-build\n")
+    target.write("POLL_MODE := -poll-mode 1\n")
     target.write("\n")
-    target.write("# Main-build Target\n")
-    target.write("main-build: ")
+
+    target.write("DMCL_CLK := -dmclkid 1\n")
+    target.write("\n")
+    
+    target.write("EMULATION := -emulation\n")
+    target.write("\n")
+    emu_mode = data.get('emu_mode')
+    target.write("EMULATION_MODE := ")
+    target.write(emu_mode)
+    target.write("\n")     
+    target.write("\n")     
+
+    target.write("# Builds Main Executable\n")
+    target.write("all: SDx-build\n")
+    target.write("\n")
+    target.write("# SDx Build Target\n")
+    target.write("SDx-build: ")
     executable = data.get("exe")
     target.write(executable)
     target.write("\n")
     target.write("\n")
-    target.write("# Tool invocations\n")
+    target.write("# SDx Compiler Build Process\n")
     target.write(executable)
     target.write(": $(OBJS) $(USER_OBJS)\n")
-    target.write("\t @echo 'Building target: $@'\n")
-    target.write("\t @echo 'Invoking: SDS++ Linker'\n")
-    target.write("\t sds++ -poll-mode 1 -o ")
+    target.write("\t @echo 'Building Target: $@'\n")
+    target.write("\t @echo 'Triggering: SDS++ Linker'\n")
+    target.write("\t $(SDSCC) $(POLL_MODE) -o ")
     target.write("\"")
     target.write(executable)
     target.write("\" ")
     if emu_switch == "Yes":
-        target.write("$(OBJS) $(USER_OBJS) $(LIBS) -dmclkid 1 -mno-bitstream -mno-boot-files -sds-sys-config linux -sds-proc a9_0 -sds-pf ")
+        target.write("$(OBJS) $(DMCL_CLK) -mno-bitstream -mno-boot-files $(OS) $(PLATFORM) ")
     else:
-        target.write("$(OBJS) $(USER_OBJS) $(LIBS) -dmclkid 1 -sds-sys-config linux -sds-proc a9_0 -sds-pf ")
+        target.write("$(OBJS) $(DMCL_CLK) $(OS) $(PLATFORM) ")
     target.write("\"")
     device = data.get("device")
     target.write(device)
     target.write("\"")
     if emu_switch == "Yes":
-        target.write(" -emulation ")
-        target.write("debug")
+        target.write(" $(EMULATION) ")
+        target.write("$(EMULATION_MODE)")
 
     target.write("\n")
-    target.write("\t @echo 'Finished building target: $@'\n")
+    target.write("\t @echo 'SDx Completed Building Target: $@'\n")
     target.write("\t @echo ' '\n")
     target.write("\n")
-    target.write("# Other Targets\n")
+
+    target.write("# Removes SDS Build Directories\n") 
+    target.write("RM := rm -rf _sds sd_card\n")
+    target.write("\n")
+
+    target.write("# Clean Target\n")
     target.write("clean:\n")
-    target.write("\t -$(RM) $(CC_DEPS) $(EXECUTABLES) $(C_UPPER_DEPS) $(OBJS) $(CXX_DEPS) $(C_DEPS) $(CPP_DEPS) ")
+    target.write("\t -$(RM) $(OBJS) $(CPP_DEPS) ")
     target.write(executable)
     target.write("\n")
     target.write("\t -@echo ' '\n")
     target.write("\n") 
     if emu_switch == "Yes": 
+        target.write("# Builds Executable and Tests Emulation Execution on QEMU\n")
         target.write("check: all\n")
         target.write("\t ../../../utility/emu_run.sh ")
         target.write(executable)
@@ -120,16 +149,8 @@ def create_mk(target, data, emu_switch):
         # Hardware check option can be included in future
         target.write("\n")
 
-    target.write("pre-build:\n")
-    target.write("\t -sdsoc_make_clean ")
-    build_flow = data.get("build_flow")
-    target.write(build_flow)
-    target.write("\n")
-    target.write("\t -@echo ' '\n")
-   
     target.write("\n") 
-    target.write(".PHONY: all clean dependents\n")
-    target.write(".SECONDARY: main-build pre-build\n")
+    target.write(".PHONY: all clean SDx-build\n")
     target.write("\n")
 
     return
@@ -145,7 +166,7 @@ if "emulation" == flow:
     os.chdir("emulation")
     os.makedirs("src")
     os.chdir("src")
-    sub_target = open("subdir.mk", "w")
+    sub_target = open("srcobj.mk", "w")
     create_subdir_mk(sub_target, data)
     sub_target.close
     os.chdir("../")
@@ -157,7 +178,7 @@ else:
     os.chdir("hardware")
     os.makedirs("src")
     os.chdir("src")
-    sub_target = open("subdir.mk", "w")
+    sub_target = open("srcobj.mk", "w")
     create_subdir_mk(sub_target, data)
     sub_target.close
     os.chdir("../")
