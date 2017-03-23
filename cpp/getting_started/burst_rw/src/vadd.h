8b9211e078d932f73b69a3fdd9c95c9699b43ff6
Kali Uday Balleda <kalib@xilinx.com>
2017-03-23 11:35:30 +0530

    [burst_rw] : Zero_copy mechanism is included

--- cpp/getting_started/burst_rw/src/vadd.h
+++ cpp/getting_started/burst_rw/src/vadd.h
@@ -6,8 +6,7 @@
 //define internal buffer max size
 #define BURSTBUFFERSIZE 2048
 
-// DMA Based
-#pragma SDS data access_pattern(a:SEQUENTIAL, out:SEQUENTIAL)
-void vadd_accel(int a[DATA_SIZE], int size, int inc_value, int out[DATA_SIZE]);
+#pragma SDS data zero_copy(a,out)
+void vadd_accel(int *a, int size, int inc_value, int *out);
 
 #endif
