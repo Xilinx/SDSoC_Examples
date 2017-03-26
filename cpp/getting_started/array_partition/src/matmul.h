92ff66a722d27f008bbc8e0563a8778e29d9a384
Kali Uday Balleda <kalib@xilinx.com>
2017-03-26 21:31:22 +0530

    [array_partition]:zero copy mechanism included

--- cpp/getting_started/array_partition/src/matmul.h
+++ cpp/getting_started/array_partition/src/matmul.h
@@ -1,4 +1,5 @@
 
+#pragma SDS data zero_copy(in1, in2, out)
 void matmul_partition_accel(int *in1,  // Read-Only Matrix 1
            int *in2,  // Read-Only Matrix 2
            int *out,        // Output Result
