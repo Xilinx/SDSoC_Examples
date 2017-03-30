e3da8a4ac330770ba6ab79eab1ee63168a4450bb
Kali Uday Balleda <kalib@xilinx.com>
2017-03-30 19:55:18 +0530

    [full_array_2d]: Copy right and length of lines

--- cpp/getting_started/full_array_2d/src/mmult.cpp
+++ cpp/getting_started/full_array_2d/src/mmult.cpp
@@ -1,31 +1,41 @@
-/**********
-Copyright (c) 2016, Xilinx, Inc.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-**********/
+/*
+# (c) Copyright 2013 - 2016 Xilinx, Inc. All rights reserved.
+#
+# This file contains confidential and proprietary information of Xilinx, Inc. and
+# is protected under U.S. and international copyright and other intellectual
+# property laws.
+# 
+# DISCLAIMER 
+# This disclaimer is not a license and does not grant any rights to the materials
+# distributed herewith. Except as otherwise provided in a valid license issued to
+# you by Xilinx, and to the maximum extent permitted by applicable law: (1) THESE
+# MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX HEREBY
+# DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
+# INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
+# FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
+# in contract or tort, including negligence, or under any other theory of
+# liability) for any loss or damage of any kind or nature related to, arising
+# under or in connection with these materials, including for any direct, or any
+# indirect, special, incidental, or consequential loss or damage (including loss
+# of data, profits, goodwill, or any type of loss or damage suffered as a result
+# of any action brought by a third party) even if such damage or loss was
+# reasonably foreseeable or Xilinx had been advised of the possibility of the
+# same.
+#
+# CRITICAL APPLICATIONS
+# Xilinx products are not designed or intended to be fail-safe, or for use in any
+# application requiring fail-safe performance, such as life-support or safety
+# devices or systems, Class III medical devices, nuclear facilities, applications
+# related to the deployment of airbags, or any other applications that could lead
+# to death, personal injury, or severe property or environmental damage
+# (individually and collectively, "Critical Applications").Customer assumes the
+# sole risk and liability of any use of Xilinx products in Critical Applications,
+# subject only to applicable laws and regulations governing limitations on product
+# liability.
+#
+# THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
+# ALL TIMES.
+*/
 
 /*******************************************************************************
 Description:
@@ -48,7 +58,8 @@ void mmult_accel(int *a, int *b, int *c, int size) {
 
 	int bufa[N][N], bufb[N][N], bufc[N][N];
 	int matrix_size = size*size;
-	// Read data from global memory and write into local buffer for a, loop pipeline will be automatically inferred
+	// Read data from global memory and write into local buffer for a, 
+	// loop pipeline will be automatically inferred
 	int x = 0, y = 0;
 	read_data_a: for (int i = 0 ; i < matrix_size ; i++){
 	#pragma HLS LOOP_TRIPCOUNT min=1 max=65536
@@ -62,7 +73,8 @@ void mmult_accel(int *a, int *b, int *c, int size) {
 		}
 	}
 
-	// Read data from global memory and write into local buffer for b, loop pipeline will be automatically inferred
+	// Read data from global memory and write into local buffer for b, loop pipeline 
+	// will be automatically inferred
 	read_data_b: for (int i = 0, x=0, y=0; i < matrix_size ; i++){
 	#pragma HLS LOOP_TRIPCOUNT min=1 max=65536
 		int tmpData_b = b[i];
@@ -75,7 +87,8 @@ void mmult_accel(int *a, int *b, int *c, int size) {
 		}
 	}
 
-	// Calculate matrix multiplication using local data buffer based on input size, and write results into local buffer for c
+	// Calculate matrix multiplication using local data buffer based on input size, 
+	// and write results into local buffer for c
 	matrix_mult: for (int row = 0; row < size; row++) {
 	#pragma HLS LOOP_TRIPCOUNT min=1 max=256
 		for (int col = 0; col < size; col++) {
@@ -89,7 +102,8 @@ void mmult_accel(int *a, int *b, int *c, int size) {
 			bufc[row][col] = result;
 		}
 	}
-	// Write results from local buffer to global memory for c, loop pipeline will be automatically inferred
+	// Write results from local buffer to global memory for c, loop pipeline will be 
+	// automatically inferred
 	int m = 0, n = 0;
 	write_data: for (int i = 0 ; i < matrix_size ; i++){
 	#pragma HLS LOOP_TRIPCOUNT min=1 max=65536
