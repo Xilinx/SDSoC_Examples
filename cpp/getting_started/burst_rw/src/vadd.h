c1a14c7c844788ee5c45eaac1f121465b8169cb2
Kali Uday Balleda <kalib@xilinx.com>
2017-03-30 17:03:31 +0530

    [burst_rw]: Copy right updated

--- cpp/getting_started/burst_rw/src/vadd.h
+++ cpp/getting_started/burst_rw/src/vadd.h
@@ -3,10 +3,9 @@
 
 #define DATA_SIZE 256
 #define INCR_VALUE 10
-//define internal buffer max size
+// Define Internal Buffer Max Size
 #define BURSTBUFFERSIZE 2048
 
 #pragma SDS data zero_copy(a,out)
 void vadd_accel(int *a, int size, int inc_value, int *out);
-
 #endif
