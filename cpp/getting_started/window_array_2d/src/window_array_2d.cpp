dd513dce29b637e8fcb31341b488140f970f76a3
Kali Uday Balleda <kalib@xilinx.com>
2017-03-30 21:08:39 +0530

    [loop_fusion-end]: copy right and line length vms

--- cpp/getting_started/window_array_2d/src/window_array_2d.cpp
+++ cpp/getting_started/window_array_2d/src/window_array_2d.cpp
@@ -1,38 +1,48 @@
-/**********
-Copyright (c) 2016, Xilinx, Inc.
-All rights reserved.
+/*
+# (c) Copyright 2013 - 2016 Xilinx, Inc. All rights reserved.
+#
+# This file contains confidential and proprietary information of Xilinx, Inc. and
+# is protected under U.S. and international copyright and other intellectual
+# property laws.
+# 
+# DISCLAIMER 
+# This disclaimer is not a license and does not grant any rights to the materials
+# distributed herewith. Except as otherwise provided in a valid license issued to
+# you by Xilinx, and to the maximum extent permitted by applicable law: (1) THESE
+# MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX HEREBY
+# DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
+# INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
+# FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
+# in contract or tort, including negligence, or under any other theory of
+# liability) for any loss or damage of any kind or nature related to, arising
+# under or in connection with these materials, including for any direct, or any
+# indirect, special, incidental, or consequential loss or damage (including loss
+# of data, profits, goodwill, or any type of loss or damage suffered as a result
+# of any action brought by a third party) even if such damage or loss was
+# reasonably foreseeable or Xilinx had been advised of the possibility of the
+# same.
+#
+# CRITICAL APPLICATIONS
+# Xilinx products are not designed or intended to be fail-safe, or for use in any
+# application requiring fail-safe performance, such as life-support or safety
+# devices or systems, Class III medical devices, nuclear facilities, applications
+# related to the deployment of airbags, or any other applications that could lead
+# to death, personal injury, or severe property or environmental damage
+# (individually and collectively, "Critical Applications").Customer assumes the
+# sole risk and liability of any use of Xilinx products in Critical Applications,
+# subject only to applicable laws and regulations governing limitations on product
+# liability.
+#
+# THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
+# ALL TIMES.
+*/
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-**********/
-
-/***************************************************************************************
+/************************************************************************************
 Description:
     
-    C Kernel Example using AXI4-master interface to access window of data from 2D array
+  C Kernel Example using AXI4-master interface to access window of data from 2D array
 
-****************************************************************************************/
+************************************************************************************/
 
 //Includes
 #include <stdio.h>
@@ -50,7 +60,8 @@ void read_data(DTYPE *inx, my_data_fifo &inFifo) {
                 rd_buf_loop_n: for (int n = 0; n < TILE_WIDTH; ++n) {
                 #pragma HLS PIPELINE
                     // should burst TILE_WIDTH in WORD beat
-                    tile[m][n] = inx[TILE_HEIGHT*TILE_PER_ROW*TILE_WIDTH*i+TILE_PER_ROW*TILE_WIDTH*m+TILE_WIDTH*j+n];
+                    tile[m][n] = inx[TILE_HEIGHT*TILE_PER_ROW*TILE_WIDTH*i
+                                    +TILE_PER_ROW*TILE_WIDTH*m+TILE_WIDTH*j+n];
                 }
             }
             rd_loop_m: for (int m = 0; m < TILE_HEIGHT; ++m) {
@@ -79,14 +90,16 @@ void write_data(DTYPE *outx, my_data_fifo &outFifo) {
             wr_loop_m: for (int m = 0; m < TILE_HEIGHT; ++m) {
                 wr_loop_n: for (int n = 0; n < TILE_WIDTH; ++n) {
                 #pragma HLS PIPELINE
-                    outx[TILE_HEIGHT*TILE_PER_ROW*TILE_WIDTH*i+TILE_PER_ROW*TILE_WIDTH*m+TILE_WIDTH*j+n] = tile[m][n];
+                    outx[TILE_HEIGHT*TILE_PER_ROW*TILE_WIDTH*i+TILE_PER_ROW
+                         *TILE_WIDTH*m+TILE_WIDTH*j+n] = tile[m][n];
                 }
             }
         }
     }
 }
 
-// Compute function, currently as simple as possible because this example is focused on efficient memory access pattern.
+// Compute function, currently as simple as possible because this example is 
+// focused on efficient memory access pattern.
 void compute(my_data_fifo &inFifo, my_data_fifo &outFifo, DTYPE alpha) {
     compute_loop_i: for(int i = 0; i < TILE_PER_COLUMN*TILE_HEIGHT; ++i) {
         compute_loop_j: for (int j = 0; j < TILE_PER_ROW; ++j) {
@@ -104,11 +117,14 @@ void compute(my_data_fifo &inFifo, my_data_fifo &outFifo, DTYPE alpha) {
 void window_array_2d_accel(DTYPE *inx, DTYPE *outx, DTYPE alpha) {
 	my_data_fifo inFifo;
 #pragma HLS stream variable=inFifo depth=4096
-// User can change the FIFO depth if array size is not 64*64, but currently we have limitation here due to some issues in tool.
-// The FIFO depth can NOT be smaller than the full array size, namely BLOCK_SIZE defined in header file "window_array_2d.h"
+// User can change the FIFO depth if array size is not 64*64, but currently we have 
+// limitation here due to some issues in tool.
+// The FIFO depth can NOT be smaller than the full array size, namely BLOCK_SIZE 
+// defined in header file "window_array_2d.h"
 	my_data_fifo outFifo;
 
-// Enables task level pipelining, allowing functions and loops to execute concurrently. Used to minimize interval. More details please refer to UG902.
+// Enables task level pipelining, allowing functions and loops to execute 
+// concurrently. Used to minimize interval. More details please refer to UG902.
 #pragma HLS DATAFLOW
 	// Read data from 2D array using tile/window pattern
 	read_data(inx, inFifo);
