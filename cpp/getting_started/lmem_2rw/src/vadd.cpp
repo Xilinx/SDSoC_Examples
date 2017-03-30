38c5de820199b7e4080efd6640848d0b2b3b530d
Kali Uday Balleda <kalib@xilinx.com>
2017-03-30 20:06:11 +0530

    [lmem_2rw]: Copy right and line length

--- cpp/getting_started/lmem_2rw/src/vadd.cpp
+++ cpp/getting_started/lmem_2rw/src/vadd.cpp
@@ -1,31 +1,41 @@
-/**********
-Copyright (c) 2016, Xilinx, Inc.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-**********/
+/*
+# (c) Copyright 2013 - 2016 Xilinx, Inc. All rights reserved.
+#
+# This file contains confidential and proprietary information of Xilinx, Inc. and
+# is protected under U.S. and international copyright and other intellectual
+# property laws.
+# 
+# DISCLAIMER 
+# This disclaimer is not a license and does not grant any rights to the materials
+# distributed herewith. Except as otherwise provided in a valid license issued to
+# you by Xilinx, and to the maximum extent permitted by applicable law: (1) THESE
+# MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX HEREBY
+# DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
+# INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
+# FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
+# in contract or tort, including negligence, or under any other theory of
+# liability) for any loss or damage of any kind or nature related to, arising
+# under or in connection with these materials, including for any direct, or any
+# indirect, special, incidental, or consequential loss or damage (including loss
+# of data, profits, goodwill, or any type of loss or damage suffered as a result
+# of any action brought by a third party) even if such damage or loss was
+# reasonably foreseeable or Xilinx had been advised of the possibility of the
+# same.
+#
+# CRITICAL APPLICATIONS
+# Xilinx products are not designed or intended to be fail-safe, or for use in any
+# application requiring fail-safe performance, such as life-support or safety
+# devices or systems, Class III medical devices, nuclear facilities, applications
+# related to the deployment of airbags, or any other applications that could lead
+# to death, personal injury, or severe property or environmental damage
+# (individually and collectively, "Critical Applications").Customer assumes the
+# sole risk and liability of any use of Xilinx products in Critical Applications,
+# subject only to applicable laws and regulations governing limitations on product
+# liability.
+#
+# THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
+# ALL TIMES.
+*/
 
 /*******************************************************************************
 Description:
@@ -79,16 +89,18 @@ void vadd_accel(
             v2_buffer[j] = in2[i + j];
         }
 
-        //FPGA implementation, local array is mostly implemented as BRAM Memory block.
-        // BRAM Memory Block contains two memory ports which allow two parallel access
-        // to memory. To utilized both ports of BRAM block, vector addition loop is
-        // unroll with factor of 2. It is equivalent to following code:
+        // FPGA implementation, local array is mostly implemented as BRAM Memory 
+        // block.
+        // BRAM Memory Block contains two memory ports which allow two parallel 
+        // access to memory. To utilized both ports of BRAM block,
+        // vector addition loop is unrolled with factor of 2. 
+        // It is equivalent to following code:
         //  for (int j = 0 ; j < chunk_size ; j+= 2){
         //    vout_buffer[j]   = v1_buffer[j] + v2_buffer[j];
         //    vout_buffer[j+1] = v1_buffer[j+1] + v2_buffer[j+1];
         //  }
-        //Which means two iterations of loop will be executed together and as a result
-        // it will double the performance.
+        // Which means two iterations of loop will be executed together and as a 
+        // resutl it will double the performance.
         vadd: for (int j = 0 ; j < chunk_size; j ++){
         #pragma HLS PIPELINE
         #pragma HLS UNROLL FACTOR=2
